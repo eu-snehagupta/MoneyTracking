9cd478d3cf975bf339da605a69181b4301694b9c
Sneha <sneha.gupta@gmail.com>
2020-10-20 20:24:01 +0200

    Add gradle to the project

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,20 @@
+/**
+ * It is a main function.
+ * Envokes the object of MainConsole class.
+ *
+ * @author Sneha Gupta
+ * @version 1.0 (2020.10.04)
+ */
+
+
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        MainConsole Obj = new MainConsole();
+        Obj.display(); //invoking the function to display on the console
+    }
+
+}
+
+
