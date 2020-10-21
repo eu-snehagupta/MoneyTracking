58b441a07af7a55b193667c4dce7feb0fc147867
Sneha <sneha.gupta@gmail.com>
2020-10-21 23:44:15 +0200

    Add Sorting implementation- sort according to user preferences.

--- src/main/java/com/company/MoneyTracking.java
+++ src/main/java/com/company/MoneyTracking.java
@@ -12,13 +12,12 @@
 
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 //import java.time.LocalDate;
 //import java.time.format.DateTimeFormatter;
 
 class Items {
-    String type;
+    private String type;
     private String title;
     //public LocalDate dateFormat;
     private String month;
@@ -34,24 +33,107 @@ class Items {
         this.amount = amount;
 
     }
+
     @Override
     public String toString() {
-
         return "" + type + "**" + title + "**" + month + "**" + amount;
+    }
 
+    public String getType() {
+        return this.type;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getMonth() {
+        return this.month;
+    }
+
+    public double getAmount() {
+        return this.amount;
     }
 }
 
 public class MoneyTracking {
 
-    public static ArrayList<Items> moneyList;
-
-    public MoneyTracking() {
-        moneyList = new ArrayList<>();
-    }
+    public static ArrayList<Items> moneyList = new ArrayList<>();
 
     public void showItems() {  //shorting is pending
 
+        System.out.println("Pick an option:");
+        System.out.println("(1) Show Items");
+        System.out.println("(2) Sort Items");
+        System.out.println("->");
+
+        int userChoice = scanUserInteger();
+
+        switch (userChoice) {
+            case 1 -> show();   //displays each items of the list.
+            case 2 -> sort();  //sort items of the list.
+            default -> System.out.println("Invalid userChoice!");
+        }
+
+        System.out.println("Enter any key to continue-->");
+        scanUserString();
+    }
+
+
+    private void sort() {
+        System.out.println("Pick an option:");
+        System.out.println("(1) Sort by title");
+        System.out.println("(2) Sort by month");
+        System.out.println("(3) Sort by amount");
+        System.out.println("->");
+
+        int userChoice = scanUserInteger();
+
+        switch (userChoice) {
+            case 1 -> printEachItem(sortByTitle());
+            case 2 -> printEachItem(sortByMonth());
+            case 3 -> printEachItem(sortByAmount());
+            default -> System.out.println("Invalid userChoice!");
+        }
+    }
+
+    private ArrayList<Items> sortByTitle() {
+        FileHandler fileHandler = new FileHandler();
+        ArrayList<Items> readData = fileHandler.readAsData();
+
+        Comparator<Items> compareByTitle = Comparator.comparing(Items::getTitle);
+        Collections.sort(readData, compareByTitle);
+
+        return readData;
+    }
+
+    private ArrayList<Items> sortByMonth() {
+        FileHandler fileHandler = new FileHandler();
+        ArrayList<Items> readData = fileHandler.readAsData();
+
+        List<String> validMonths = Arrays.asList("january", "february", "march", "april", "may", "june",
+                "july", "august", "september", "october", "november", "december");
+
+        Comparator<Items> compareByMonth = Comparator.comparing(item -> Integer.valueOf(validMonths.indexOf(item.getMonth())));
+        Collections.sort(readData, compareByMonth);
+
+        //Comparator<Items> compareByMonth = Comparator.comparing(Items::getMonth);
+        //Collections.sort(readData, compareByMonth);
+
+        return readData;
+    }
+
+    private ArrayList<Items> sortByAmount() {
+        FileHandler fileHandler = new FileHandler();
+        ArrayList<Items> readData = fileHandler.readAsData();
+
+        Comparator<Items> compareByAmount = Comparator.comparingDouble(Items::getAmount);
+        Collections.sort(readData, compareByAmount);
+
+        return readData;
+    }
+
+    private void show() {
         System.out.println("Pick an option:");
         System.out.println("(1) All");
         System.out.println("(2) Show Expense");
@@ -66,8 +148,6 @@ public class MoneyTracking {
             case 3 -> printEachItem(filterByType("Income"));   // need to handle that case.
             default -> System.out.println("Invalid userChoice!");
         }
-        System.out.println("Enter any key to continue..");
-        scanUserString();
     }
 
     public void addItems() {
@@ -99,7 +179,7 @@ public class MoneyTracking {
         moneyList.add(obj);             //holds the values of the item in moneylist, till its saved using savequit feature.
         System.out.println("Item added successfully!");
 
-        System.out.println("Enter any key to continue..");
+        System.out.println("Enter any key to continue-->");
         scanUserString();
     }
 
@@ -172,7 +252,7 @@ public class MoneyTracking {
             return readData;
         } else {
             for (Items objData : readData) {
-                if ((objData.type).equals(filterValue)) {
+                if ((objData.getType()).equals(filterValue)) {
                     showData.add(objData);
                 }
             }
@@ -183,17 +263,17 @@ public class MoneyTracking {
     private void editValue(ArrayList<Items> showData, String editType) {
 
         System.out.println("Pick an option:");
-        for (int i = 1; i <= showData.size(); i++) {
+        for (int i = 0; i < showData.size(); i++) {
             System.out.println(i + ": " + showData.get(i));  //choice from 1,.. option, which list item to edit
         }
 
         int userChoice = scanUserInteger();
 
-        System.out.println("Enter your Title:");  //ask user to re-enter the values for title, date, amount
+        System.out.println("Enter new title:");  //ask user to re-enter the values for title, date, amount
         String editTitle = scanUserString();        //should it be an optional feature??
-        System.out.println("Enter your Date(yyyy-MM-dd):");   //to ask user to enter only those values which he/she wish to edit
-        String editDate = scanUserString();
-        System.out.println("Enter your Amount:");
+        System.out.println("Enter new month:");   //to ask user to enter only those values which he/she wish to edit
+        String editDate = scanUserMonth();
+        System.out.println("Enter new amount:");
         double editAmount = scanUserDouble();
 
         for (int i = 0; i < showData.size(); i++) {
@@ -201,12 +281,13 @@ public class MoneyTracking {
                 showData.set(i, new Items(editType, editTitle, editDate, editAmount));
             }
         }
+        System.out.println(showData);
     }
 
     private void removeValue(ArrayList<Items> showData) {
         int userChoice;
         System.out.println("Pick an option:");  //choice from 1,.. option, which list item to remove
-        for (int i = 1; i <= showData.size(); i++) {
+        for (int i = 0; i < showData.size(); i++) {
             System.out.println(i + ": " + showData.get(i));
         }
 
@@ -241,8 +322,8 @@ public class MoneyTracking {
         return inputString;
     }
 
-    private String scanUserMonth(){
-        String month =scanUserString().toLowerCase();
+    private String scanUserMonth() {
+        String month = scanUserString().toLowerCase();
         String[] validMonths = {"january", "february", "march", "april", "may", "june",
                 "july", "august", "september", "october", "november", "december"};
         boolean isValid = false;
@@ -252,7 +333,7 @@ public class MoneyTracking {
                 break;
             }
         }
-        if (!isValid){
+        if (!isValid) {
             System.out.println("Invalid month. Re-enter the month:");
             scanUserMonth();
         }
